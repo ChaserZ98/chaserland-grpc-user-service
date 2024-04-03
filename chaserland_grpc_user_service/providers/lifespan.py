c62bf9f1720887aa716971bd204ae1f4b53d8d66
ChaserZ98 <feiyuzheng98@gmail.com>
2024-04-03 19:21:02 -0400

    ✨ feat: Add lifespan provider for gRPC server

---
+++ chaserland_grpc_user_service/providers/lifespan.py
@@ -0,0 +1,26 @@
+from contextlib import asynccontextmanager
+from typing import AsyncIterator
+
+from chaserland_grpc_user_service.utils.AIOgRPCServer import AIOgRPCServer, logger
+from chaserland_grpc_user_service.utils.provider import Provider
+
+
+async def on_startup(app: AIOgRPCServer):
+    logger.info("Starting server on %s", app.address)
+
+
+async def on_shutdown(app: AIOgRPCServer):
+    logger.info("Server stopped.")
+
+
+@asynccontextmanager
+async def lifespan(app: AIOgRPCServer) -> AsyncIterator[dict]:
+    await on_startup(app)
+    yield {}
+    await on_shutdown(app)
+
+
+class LifespanProvider(Provider):
+    @staticmethod
+    def register(app: AIOgRPCServer):
+        app.set_lifespan(lifespan)
