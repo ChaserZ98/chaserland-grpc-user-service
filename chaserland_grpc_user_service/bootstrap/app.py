38902c64c3520a551121f65b07bd38efa30cc636
ChaserZ98 <feiyuzheng98@gmail.com>
2024-04-04 02:35:07 -0400

    ✨ feat: Include log interceptor and exception handler interceptor in bootstrap

--- chaserland_grpc_user_service/bootstrap/app.py
+++ chaserland_grpc_user_service/bootstrap/app.py
@@ -2,11 +2,16 @@ from chaserland_grpc_user_service.providers.lifespan import LifespanProvider
 from chaserland_grpc_user_service.providers.log import LoggingProvider
 from chaserland_grpc_user_service.providers.servicer import ServicerProvider
 from chaserland_grpc_user_service.utils.AIOgRPCServer import AIOgRPCServer, logger
+from chaserland_grpc_user_service.utils.interceptors import (
+    AsyncAccessLoggerInterceptor,
+    AsyncExceptionToStatusInterceptor,
+)
 from chaserland_grpc_user_service.utils.provider import Provider
 
 
 def create_server() -> AIOgRPCServer:
-    server = AIOgRPCServer()
+    interceptors = [AsyncAccessLoggerInterceptor(), AsyncExceptionToStatusInterceptor()]
+    server = AIOgRPCServer(interceptors=interceptors)
     register(server, LoggingProvider)
     register(server, LifespanProvider)
     register(server, ServicerProvider)
