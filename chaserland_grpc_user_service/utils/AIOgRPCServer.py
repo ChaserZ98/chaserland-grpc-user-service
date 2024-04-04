aace374d87ddea198939e8b8cf67eb89f51a5f95
ChaserZ98 <feiyuzheng98@gmail.com>
2024-04-04 02:27:43 -0400

    ✨ feat: Add interceptors to AIOgRPCServer constructor

--- chaserland_grpc_user_service/utils/AIOgRPCServer.py
+++ chaserland_grpc_user_service/utils/AIOgRPCServer.py
@@ -1,6 +1,6 @@
 import asyncio
 import logging
-from typing import AsyncIterator
+from typing import AsyncIterator, List
 
 import grpc
 
@@ -17,8 +17,9 @@ class AIOgRPCServer:
         address: str = app_settings.server_address,
         graceful_shutdown_timeout: int = 5,
         lifespan: callable = None,
+        interceptors: List[grpc.ServerInterceptor] = None,
     ):
-        self.server = grpc.aio.server()
+        self.server = grpc.aio.server(interceptors=interceptors)
         self.address = address
         self.graceful_shutdown_timeout = graceful_shutdown_timeout
         self.cleanup_coroutines = []
