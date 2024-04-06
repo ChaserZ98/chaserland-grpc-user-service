16df2a9bbaef7303f43f5497962416b61f459bdf
ChaserZ98 <feiyuzheng98@gmail.com>
2024-04-06 00:40:01 -0400

    ✨ feat: Add OAuthProvider abstract class for latter implementation of various OAuth types

--- src/utils/provider.py
+++ src/utils/provider.py
@@ -1,4 +1,19 @@
 from abc import ABC, abstractmethod
+from typing import Any
+
+
+class OAuthProvider(ABC):
+    def __init__(self, client_id: str, client_secret: str):
+        self.client_id = client_id
+        self.client_secret = client_secret
+
+    @abstractmethod
+    async def get_token(self, code: str, session: Any):
+        raise NotImplementedError
+
+    @abstractmethod
+    async def check_token(self, access_token: str, session: Any):
+        raise NotImplementedError
 
 
 class Provider(ABC):
