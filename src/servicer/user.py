adabf8509876d0d941849d4e2d0c7c8fdce7d1c8
ChaserZ98 <feiyuzheng98@gmail.com>
2024-04-06 03:10:01 -0400

    ✨ feat: Extend UserServicer to further inherit AIOgRPCServicer

--- src/servicer/user.py
+++ src/servicer/user.py
@@ -3,8 +3,13 @@ import grpc_interceptor.exceptions as grpc_exceptions
 from chaserland_grpc_proto.protos.user import user_pb2 as user_message
 from chaserland_grpc_proto.protos.user import user_pb2_grpc as user_service
 
+from src.utils.servicer import AIOgRPCServicer
 
-class UserServicer(user_service.UserServicer):
+
+class UserServicer(user_service.UserServicer, AIOgRPCServicer):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
     async def oauth_login(
         self, request: user_message.OAuthLoginRequest, context: grpc.ServicerContext
     ) -> user_message.OAuthLoginResponse:
