50d0a8159c884c6042153a1345d22b9f28e9d44b
ChaserZ98 <feiyuzheng98@gmail.com>
2024-04-07 19:54:11 -0400

    🎉 feat: Add GithubOAuthProvider to UserServicer in servicer.py

--- src/providers/servicer.py
+++ src/providers/servicer.py
@@ -1,5 +1,6 @@
 from chaserland_grpc_proto.protos.user import user_pb2_grpc as user_service
 
+from src.providers.oauth import GithubOAuthProvider
 from src.servicer.user import UserServicer
 from src.utils.AIOgRPCServer import AIOgRPCServer
 from src.utils.provider import Provider
@@ -8,7 +9,12 @@ from src.utils.provider import Provider
 class ServicerProvider(Provider):
     @staticmethod
     def register(server: AIOgRPCServer) -> None:
+        oauth_providers = [
+            GithubOAuthProvider(),
+        ]
+        servicer = UserServicer(server_context_ref=server.context_ref)
+        servicer.add_oauth_providers(oauth_providers)
         server.add_servicer(
             user_service.add_UserServicer_to_server,
-            UserServicer(server_context_ref=server.context_ref),
+            servicer,
         )
