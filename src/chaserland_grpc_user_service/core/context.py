18df33787f4bcb3274797f92ac1c9f311a72e51a
ChaserZ98 <feiyuzheng98@gmail.com>
2024-05-02 00:28:23 -0400

    🦄 refactor: Refactor Context class to use built-in dataclass instead of Pydantic

--- src/chaserland_grpc_user_service/core/context.py
+++ src/chaserland_grpc_user_service/core/context.py
@@ -1,22 +1,18 @@
+from dataclasses import dataclass
 from typing import Self
 
 import aiohttp
 from chaserland_common.grpc.aio import AbstractContext
-from pydantic import BaseModel, ConfigDict, Field
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker
 
 from ..db.database import async_session
 
 
-class Context(BaseModel, AbstractContext):
-    model_config = ConfigDict(arbitrary_types_allowed=True)
+@dataclass(frozen=True)
+class Context(AbstractContext):
+    http_session: aiohttp.ClientSession = aiohttp.ClientSession()
 
-    http_session: aiohttp.ClientSession = Field(
-        frozen=True, default_factory=aiohttp.ClientSession
-    )
-    db_session: async_sessionmaker[AsyncSession] = Field(
-        frozen=True, default_factory=async_session
-    )
+    db_session: async_sessionmaker[AsyncSession] = async_session
 
     async def __aenter__(self) -> Self:
         return self
